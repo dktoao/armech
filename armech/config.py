7eabf02cb93c43ef3ca6c7fde89e43b3f21866fd
Daniel Kuntz <daniel.kuntz@dk-cloud.net>
2016-02-25 06:59:07 -0700

    Added transforms to the Link class

---
+++ armech/config.py
@@ -0,0 +1,7 @@
+## config.py
+#
+# Constants used by the program to identify settings and other configurations
+
+# Joint Type Constants
+JOINT_REVOLUTE = 1
+JOINT_PRISMATIC = 2
--- armech/core/link.py
+++ armech/core/link.py
@@ -6,16 +6,20 @@
 # MAJOR TODO: rewrite this whole class using the GraphicalBody or PhysicalBody class
 #
 
-from numpy import array, float_, cos, sin
+from numpy import float_, cos, sin
 
+from armech.config import JOINT_REVOLUTE, JOINT_PRISMATIC
 from armech.core.rigidbody import RigidBody
 
 
-class Link(RigidBody):
+# JointType class for consistency
 
-    def __init__(self, a, alpha, d, theta, joint_type):
+class LinkDH(RigidBody):
+
+    def __init__(self, a=0.0, alpha=0.0, d=0.0, theta=0.0, joint_type):
         """
-        A link in a serial chain robot
+        A link in a serial chain robot described by Denavit-Hartenberg (DH)
+        parameters.
         :param a: the distance from z(i) to z(i+1) measured along x(i) (meters)
         :param alpha: the angle from z(i) to z(i+1) measured about x(i) (radians)
         :param d: the distance from x(i-1) to x(i) measured along z(i) (meters)
@@ -32,27 +36,47 @@ class Link(RigidBody):
         self.alpha = float_(alpha)
         self.d = float_(d)
         self.theta = float_(theta)
-        if self.joint_type == 'revolute':
-            self.tform = self.get_revolute_tform()
-        elif self.joint_type == 'prismatic':
-            self.tform = self.prismatic_tform
+        self.tform = self.get_tform()
+        if joint_type == JOINT_REVOLUTE:
+            self.joint_type = JOINT_REVOLUTE
+            self.joint_type_str = 'Revolute Joint'
+        elif joint_type == JOINT_PRISMATIC:
+            self.joint_type = JOINT_REVOLUTE
+            self.joint_type_str = 'Prismatic Joint'
         else:
-            raise ValueError('type must be either "revolute" or "prismatic"')
+            raise ValueError(
+                'type must be either constants.JOINT_REVOLUTE or '
+                'constants.JOINT_PRISMATIC'
+            )
 
-    def get_revolute_tform(self):
+    def get_tform(self):
         """
         Return the transform from the end of the previous joint to the end of
         this joint as a function of the joint angle theta
 
-        Returns: a function tform(theta)
+        Returns: a function tform(q) where q is the state of the joint
+                 (Angular or Linear displacement)
         """
-        # Get values
-        a = self.a
-        alpha = self.alpha
-        d = self.d
-
-        def tform(theta):
-            array([
-                [cos(theta), sin(theta), 0.0, a],
-                [-sin(theta)*cos(alpha), cos(alpha)]
+        if self.joint_type == JOINT_REVOLUTE:
+            return lambda q: float_([
+                [cos(self.theta + q), sin(self.theta + q)*cos(self.alpha),
+                 sin(self.alpha)*sin(self.theta + q), self.a*cos(self.theta + q)],
+                [-sin(self.theta + q), cos(self.alpha)*cost(self.theta + q),
+                 sin(self.alpha)*cos(self.theta + q), -self.a*sin(self.theta + q)],
+                [0, -sin(self.alpha), cos(self.alpha), self.d],
+                [0, 0, 0, 1],
             ])
+        elif self.joint_type == JOINT_PRISMATIC:
+            return lambda q: float_([
+                [cos(self.theta), sin(self.theta)*cos(self.alpha + q),
+                 sin(self.alpha + q)*sin(self.theta), self.a*cos(self.theta)],
+                [-sin(self.theta), cos(self.alpha + q)*cost(self.theta),
+                 sin(self.alpha + q)*cos(self.theta), -self.a*sin(self.theta)],
+                [0, -sin(self.alpha + q), cos(self.alpha + q), self.d],
+                [0, 0, 0, 1],
+            ])
+        else:
+            raise ValueError(
+                'type must be either constants.JOINT_REVOLUTE or '
+                'constants.JOINT_PRISMATIC'
+            )
