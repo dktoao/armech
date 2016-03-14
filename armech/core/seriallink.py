11756120f04ef4c6bb06f525e9b2ce9cb64bd7fa
Daniel Kuntz <daniel.kuntz@dk-cloud.net>
2016-03-14 17:46:37 -0600

    Added transform facilities to SerialLink class

--- armech/core/seriallink.py
+++ armech/core/seriallink.py
@@ -4,7 +4,7 @@
 # serial link robot. Provides functions for forward kinematics, inverse
 # kinematics, and dynamics calculations.
 
-from numpy import identity, dot, zeros
+from numpy import identity, dot, zeros, concatenate
 
 
 class SerialLink:
@@ -20,8 +20,33 @@ class SerialLink:
         self.links = links
         self.base = base
         self.state = zeros(len(links))
-        self.rotation = identity(3)
-        self.translation = zeros((3, 1))
+        self.global_transform = identity(4)
+
+    def set_global_transform(self, rotation=None, translation=None):
+        """Set the global transform for the overall arm assembly
+
+        Args:
+            rotation: [3x3] symmetric float array describing the rotation of
+                      the robot.
+            translation: [3x1] float array describing the x,y,z position of
+                         the robot.
+
+        """
+
+        # Set input parameters to the correct type
+        if rotation is not None:
+            rotation = float_(rotation).reshape(3, 3)
+        else:
+            rotation = self.global_transform[0:3, 0:3]
+        if translation is not None:
+            translation = float_(translation).reshape(3, 1)
+        else:
+            translation = self.global_transform[0:3, 3]
+
+        self.global_transform = concatenate(
+            concatenate((rotation, translation), axis=1),
+            float_([0.0, 0.0, 0.0, 1.0]).reshape(1, 4)
+        )
 
     def move_joints(self, q):
         """
@@ -34,7 +59,7 @@ class SerialLink:
         self.check_q(q)
 
         # Apply all transforms one by one
-        global_transform = identity(4)
+        global_transform = self.global_transform
         for k, link in enumerate(self.links):
             global_state_transform = dot(
                 global_transform, link.state_transform(q[k])
@@ -45,11 +70,13 @@ class SerialLink:
             )
             global_transform = dot(global_state_transform, link.body_transform)
 
-    def get_tool_trans(self, q):
+    def get_tool_trans(self, q, local=True):
         """Get the transform of the tool from the base of the robot given the
         state configuration "q"
         Args:
             q: state vector of the robot in meters and/or radians
+            local: bool, get transform local to the robot, if False will give
+                   the transform from the robots global_coordinates
 
         Returns: 4x4 transform matrix for the end of the arm
         """
@@ -57,8 +84,13 @@ class SerialLink:
         # Check inputs
         self.check_q(q)
 
-        # Get the transform
-        transform = identity(4)
+        # Set base transform
+        if local:
+            transform = identity(4)
+        else:
+            transform = self.global_transform
+
+        # Loop through links and calculate transform
         for k, link in enumerate(self.links):
             transform = dot(
                 transform, link.state_transform(q[k])
--- armech/demo/robot.py
+++ armech/demo/robot.py
@@ -4,6 +4,7 @@
 # and geometries. For demonstration and testing purposes
 
 # Package imports
+from os.path import dirname, realpath, join
 from numpy import pi
 
 # Local imports
@@ -12,6 +13,9 @@ from armech.core.linkdh import LinkDH
 from armech.config import JOINT_REVOLUTE
 from armech.graphics.graphicalbody import GraphicalBody
 
+
+# Get the path to the cad directory
+CAD_DIR = join(dirname(realpath(__file__)), 'cad')
 
 class Simple3DOF(SerialLink):
     """A simple 3 degree of freedom robot class that can be used for testing
@@ -32,12 +36,16 @@ class Simple3DOF(SerialLink):
 
         # Create links
         base = GraphicalBody()
-        link0 = LinkDH(JOINT_REVOLUTE, 0, pi/2, 0, 0)
-        link1 = LinkDH(JOINT_REVOLUTE, 0.4, 0, 0.04, 0)
-        link2 = LinkDH(JOINT_REVOLUTE, 0.35, 0, -0.08, 0)
+        link1 = LinkDH(JOINT_REVOLUTE, 0, pi/2, 0, 0)
+        link2 = LinkDH(JOINT_REVOLUTE, 0.4, 0, 0.04, 0)
+        link3 = LinkDH(JOINT_REVOLUTE, 0.35, 0, -0.08, 0)
+
+        # Load graphics for each link
+        obj_dir = join(CAD_DIR, 'simple3dof', 'obj')
+        base.load_obj(join(obj_dir, 'base.obj'), [0.5, 0.5, 0.5])
+        link1.load_obj(join(obj_dir, 'link1.obj'), [1.0, 0.0, 0.0])
+        link2.load_obj(join(obj_dir, 'link2.obj'), [0.0, 1.0, 0.0])
+        link3.load_obj(join(obj_dir, 'link3.obj'), [0.0, 0.0, 1.0])
 
         # Initialize the SerialLink super class
-        super(Simple3DOF, self).__init__([link0, link1, link2], base)
-
-        # Load the graphics
-        # TODO: implement the rest
+        super(Simple3DOF, self).__init__([link1, link2, link3], base)
